c5e6e7931412d005dfca2a7aa2a7c68edbe3492e
Carlos Fortunato <carlosfortunatodev@gmail.com>
2024-01-30 02:19:09 -0300

    Adicionando range de tamanho de senha

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,17 +1,23 @@
+let passwordLength = 16
+
 const generatePassword = () =>{
     const chars = "abcdefghjkmnpqrstuvwxyzABCDEFGHJKLMNPQRSYUVWXYZ123456789?!@&*()[]"
 
     let password = ""
 
-    for (let i = 0; i < 8; i++) {
+    for (let i = 0; i < passwordLength; i++) {
         const randomNumber = Math.floor(Math.random() * chars.length)
         password += chars.substring(randomNumber, randomNumber+1)
     }
 
     const inputEl = document.querySelector("#password")
     inputEl.value = password
-    console.log(password);
 }
 
+const passwordLengthEl = document.querySelector('#password-length')
+passwordLengthEl.addEventListener('input', () => {
+    passwordLength = passwordLengthEl.value
+    generatePassword()
+})
 
 generatePassword()
